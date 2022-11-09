b448059c9430e084296637f0a44277a2f6229931
DarioRossi99 <rssdra99@gmail.com>
2022-11-09 18:12:28 +0100

    fine es

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 const buttonGriglia = document.getElementById("pulsanteGriglia");
-const containerGriglia = document.querySelector(".grid-container");
 
 buttonGriglia.addEventListener ("click", function () {
 
@@ -8,16 +7,32 @@ buttonGriglia.addEventListener ("click", function () {
 });
 
 /**
- * 
  * @param {number} numeroCelle 
  */
 
 function generatoreGriglia(numeroCelle){
 
+    const containerGriglia = document.querySelector(".grid-container");
     const celleTotali = numeroCelle * numeroCelle;
 
-    for (let i = 0; i < celleTotali; i++) {
-        containerGriglia.innerHTML += '<div class="grid-cell"></div>';
-        
+    for (let i = 1; i < celleTotali; i++) {
+        // containerGriglia.innerHTML += '<div class="grid-cell"></div>';
+        // const cellaSelezionata = containerGriglia.querySelector(".grid—cell:last—child");
+        // cellaSelezionata.addEventListener ("click")
+        const cellaSelezionata = document.createElement("div");
+
+        cellaSelezionata.classList.add("grid-cell")
+        cellaSelezionata.style.flexBasis = 'calc(100% / ${numeroCelle})';
+        cellaSelezionata.style.width = 'calc(100% / ${numeroCelle})';
+        cellaSelezionata.addEventListener("click", function(){
+            this.classList.toggle("bg-info");
+            console.log("hai cliccato" + i)
+        })
+
+        containerGriglia.append(cellaSelezionata);
+
     }
+
+    
+       
 }
